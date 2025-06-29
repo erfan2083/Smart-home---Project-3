6d020db4a7fea9dc1e6eaddf1fc8999454c0b065
Erfan Manouchehri <erfan2004@yahoo.com>
2025-06-29 20:40:39 +0330

    made some chnages

---
+++ src/htmlPage.h
@@ -0,0 +1,15 @@
+#ifndef HTML_PAGE_H
+#define HTML_PAGE_H
+
+const char smartHomeHTML[] PROGMEM = R"rawliteral(
+<!DOCTYPE html>
+<html>
+  <head><title>Smart Home</title></head>
+  <body>
+    <h1>Smart Home Control</h1>
+    <!-- بقیه HTML شما -->
+  </body>
+</html>
+)rawliteral";
+
+#endif
